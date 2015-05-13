0f93a5f89d66c9ebf88feebce79e95702294f9b9
Trent Swanson <trent@fullscale180.com>
2015-05-13 13:44:00 -0700

    v2.2.3

--- src/SemanticLogging.Elasticsearch/Properties/AssemblyInfo.cs
+++ src/SemanticLogging.Elasticsearch/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Security;
 [assembly: SecurityTransparent]
 
 [assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.2.2")]
-[assembly: AssemblyInformationalVersion("2.2.2")]
+[assembly: AssemblyFileVersion("2.2.3")]
+[assembly: AssemblyInformationalVersion("2.2.3")]
 
 [assembly: ComVisible(false)]
 
